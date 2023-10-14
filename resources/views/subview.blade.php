660129c66e100d0b5f9fb118d138ea9c9fc451af
arturo2 <arturo2@MacBook-Air-de-Arturo.local>
2023-10-13 19:09:43 -0600

    feat(subviews)::sparkles:Create subview

---
+++ resources/views/subview.blade.php
@@ -0,0 +1,3 @@
+<h1>Subvista 1</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere aliquam vitae expedita odio error doloremque consectetur eum maiores. Fugit nemo doloremque, neque impedit tempora explicabo culpa! Fuga architecto hic fugit!</p>
