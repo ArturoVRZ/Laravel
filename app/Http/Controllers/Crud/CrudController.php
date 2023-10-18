af09ecbeb508381e173290944722d5ccb2bec7af
arturo2 <arturo2@MacBook-Air-de-Arturo.local>
2023-10-17 18:28:35 -0600

    feat(crud)::sparkless:save and update of database

--- app/Http/Controllers/Crud/CrudController.php
+++ app/Http/Controllers/Crud/CrudController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Crud;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PutCrudRequest;
 use App\Http\Requests\StoreCrudRequest;
 use App\Models\Category;
 use App\Models\Crud;
@@ -13,15 +14,18 @@ class CrudController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() //nos ayuda a regresar una pagina con los datos
     {
         //
+        //$datos = Crud::get();
+        $datos = Crud::paginate(1);
+        return view('crud.index',['datos' => $datos]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() //nos ayuda a regresar una pagina con el formulario para llamar luego a store y hacer un post
     {
         //buscamos las categorias para enviarlas a la vsita  
         $categories = Category::get();
@@ -32,7 +36,7 @@ class CrudController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCrudRequest $request)
+    public function store(StoreCrudRequest $request) //se llama desde create y hace la carga a la base de datos
     {
         Crud::create($request->all());
     }
@@ -48,17 +52,20 @@ class CrudController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Crud $crud)
+    public function edit(Crud $post) //nos ayuda a cargar una pagina con el formulario y los datos anteriores para luego llamar a update
     {
-        //
+        // 
+        $categories = Category::get();
+        echo view('crud.edit', compact('categories', 'post'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Crud $crud)
+    public function update(PutCrudRequest $request, Crud $post) //actualiza la base de datos
     {
         //
+        $post->update($request->all());
     }
 
     /**
